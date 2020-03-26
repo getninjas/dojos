302ede31adf3357d68b93bb5da994e4bfa60e485
Lucas Pimenta <lucas.pimenta94@outlook.com>
2020-03-26 18:38:21 -0300

    add style on message

--- react-todo-list/src/components/AddItemForm/index.js
+++ react-todo-list/src/components/AddItemForm/index.js
@@ -16,9 +16,9 @@ const AddItemForm = ({
       }
     }>
       <div>
-        <input className="input input__item" value={valueInput} onChange={e => changeValue(e)}
+        <input className={`input input__item ${hasError ? '' : ''}`} value={valueInput} onChange={e => changeValue(e)}
         type="text" placeholder={placeholder} />
-        {hasError ? (<span className="input__item--invalid" style={{ 'color': 'red', 'display': 'block' }}>Você não pode inserir uma tarefa vazia</span>) : ''}
+        {hasError ? (<span className="input__item--invalid">Você não pode inserir uma tarefa vazia</span>) : ''}
       </div>
     </form>
   )
