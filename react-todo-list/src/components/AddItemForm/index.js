8c86adcd0bf160727bddea7b48e061e294a395d9
Caio Luan <caioluanc@hotmail.com>
2020-03-26 18:48:52 -0300

    updating error message

--- react-todo-list/src/components/AddItemForm/index.js
+++ react-todo-list/src/components/AddItemForm/index.js
@@ -16,9 +16,9 @@ const AddItemForm = ({
       }
     }>
       <div>
-        <input className={`input input__item ${hasError ? '' : ''}`} value={valueInput} onChange={e => changeValue(e)}
+        <input className={`input input__item ${hasError ? 'input__item--invalid' : ''}`} value={valueInput} onChange={e => changeValue(e)}
         type="text" placeholder={placeholder} />
-        {hasError ? (<span className="input__item--invalid">Você não pode inserir uma tarefa vazia</span>) : ''}
+        {hasError ? (<span className="input__message input__message--invalid">Você não pode inserir uma tarefa vazia</span>) : ''}
       </div>
     </form>
   )
